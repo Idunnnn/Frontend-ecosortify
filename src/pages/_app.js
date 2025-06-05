5cb46eef5fdb976066f0f390edf9af5d520edbe2
Idunnn <faridzakir9@gmail.com>
2025-06-06 05:15:04 +0800

    memperbaiki favicon yang tidak tampil

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,20 @@
+import Head from "next/head";
 import { UserProvider } from "@/contexts/UserContext";
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
   return (
     <UserProvider>
-     
+      <Head>
+        <title>EcoSortify</title>
+        <meta name="description" content="Website edukasi tentang pengelolaan sampah yang kekinian dan lucu" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
       <Component {...pageProps} />
-    </UserProvider>);
-
+    </UserProvider>
+  );
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,15 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>EcoSortify</title>
-        <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#000000" />
-        <meta name="description" content="Website edukasi tentang pengelolaan sampah yang kekinian dan lucu" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="icon" type="image/png" href="/favicon.png" />
-      </Head>
+      <Head></Head>
       <body className="antialiased">
         <Main />
         <NextScript />
